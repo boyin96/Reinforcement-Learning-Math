b91ceeb8ba3742f16eeada9e21675bcbbded8416
Bo Yin <38563813+boyin96@users.noreply.github.com>
2024-12-09 11:53:54 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RL'
+copyright = '2024, Bo'
+author = 'Bo Yin'
 
 release = '0.1'
 version = '0.1.0'
