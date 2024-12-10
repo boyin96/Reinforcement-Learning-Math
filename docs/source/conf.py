9c8102c099e991852750467c0d7cdc9f226f9d8f
Bo Yin <38563813+boyin96@users.noreply.github.com>
2024-12-10 11:36:57 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,10 +110,10 @@ latex_elements = {
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'spinningup', 'Spinning Up Documentation',
-     [author], 1)
-]
+# man_pages = [
+#     (master_doc, 'spinningup', 'Spinning Up Documentation',
+#      [author], 1)
+# ]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
