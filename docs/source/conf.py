7696ec5a1e82b749b4b88932ce6bcc1ff145092e
Bo Yin <38563813+boyin96@users.noreply.github.com>
2024-12-10 11:22:15 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,5 +36,69 @@ html_theme_options = {
     'navigation_depth': 4,
 }
 
+# -- Options for LaTeX output 
+
+
+imgmath_latex_preamble = r'''
+\usepackage{algorithm}
+\usepackage{algorithmic}
+\usepackage{amsmath}
+\usepackage{cancel}
+
+\usepackage[verbose=true,letterpaper]{geometry}
+\geometry{
+    textheight=12in,
+    textwidth=6.5in,
+    top=1in,
+    headheight=12pt,
+    headsep=25pt,
+    footskip=30pt
+    }
+
+\newcommand{\E}{{\mathrm E}}
+
+\newcommand{\underE}[2]{\underset{\begin{subarray}{c}#1 \end{subarray}}{\E}\left[ #2 \right]}
+
+\newcommand{\Epi}[1]{\underset{\begin{subarray}{c}\tau \sim \pi \end{subarray}}{\E}\left[ #1 \right]}
+'''
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    'preamble': r'''
+\usepackage{algorithm}
+\usepackage{algorithmic}
+\usepackage{amsmath}
+\usepackage{cancel}
+
+
+\newcommand{\E}{{\mathrm E}}
+
+\newcommand{\underE}[2]{\underset{\begin{subarray}{c}#1 \end{subarray}}{\E}\left[ #2 \right]}
+
+\newcommand{\Epi}[1]{\underset{\begin{subarray}{c}\tau \sim \pi \end{subarray}}{\E}\left[ #1 \right]}
+''',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+# # Grouping the document tree into LaTeX files. List of tuples
+# # (source start file, target name, title,
+# #  author, documentclass [howto, manual, or own class]).
+# latex_documents = [
+#     (master_doc, 'SpinningUp.tex', 'Spinning Up Documentation',
+#      'Joshua Achiam', 'manual'),
+# ]
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
